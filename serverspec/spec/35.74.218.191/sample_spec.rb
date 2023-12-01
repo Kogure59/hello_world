94ee5a20dcf113be8ca10dffbcb077b58addb5b7
Kogure59 <137606211+Kogure59@users.noreply.github.com>
2023-12-01 22:57:07 +0900

    fix: serverspecディレクトリ作り直し、config.yml修正

--- serverspec/spec/35.74.218.191/sample_spec.rb
+++ serverspec/spec/35.74.218.191/sample_spec.rb
@@ -6,12 +6,6 @@ describe command('ruby -v') do
   its(:stdout) { should match /ruby 3\.1\.2/ }
 end
 
-# Bundlerのバージョンが2.3.14か確認する
-describe command('bundler -v') do
-  let(:disable_sudo) { true }
-  its(:stdout) { should match /Bundler version 2\.3\.14/ }
-end
-
 # 複数のパッケージがインストールされているかまとめて確認する
 %w{git make gcc-c++ patch curl openssl-devel libcurl-devel libyaml-devel libffi-devel libicu-devel libxml2 libxslt libxml2-devel libxslt-devel zlib-devel readline-devel ImageMagick ImageMagick-devel }.each do |pkg|  
   describe package(pkg) do
