93c464542876a8e90270e0f03a0a64bb4dcaffc7
Kogure59 <137606211+Kogure59@users.noreply.github.com>
2023-12-02 20:44:58 +0900

    fix: playbookに合わせるためspec.rbを修正

--- serverspec/spec/35.74.218.191/sample_spec.rb
+++ serverspec/spec/35.74.218.191/sample_spec.rb
@@ -1,13 +1,12 @@
 require 'spec_helper'
 
-# Rubyのバージョンが3.1.2か確認する
-describe command('ruby -v') do
-  let(:disable_sudo) { true }
-  its(:stdout) { should match /ruby 3\.1\.2/ }
+# gitがインストールされているか確認する
+describe package('git') do
+  it { should be_installed }
 end
 
-# 複数のパッケージがインストールされているかまとめて確認する
-%w{git make gcc-c++ patch curl openssl-devel libcurl-devel libyaml-devel libffi-devel libicu-devel libxml2 libxslt libxml2-devel libxslt-devel zlib-devel readline-devel ImageMagick ImageMagick-devel }.each do |pkg|  
+# 他の複数のパッケージがインストールされているかまとめて確認する
+%w{make gcc-c++ patch curl openssl-devel libcurl-devel libyaml-devel libffi-devel libicu-devel libxml2 libxslt libxml2-devel libxslt-devel zlib-devel readline-devel ImageMagick ImageMagick-devel}.each do |pkg|  
   describe package(pkg) do
     it { should be_installed }
   end
